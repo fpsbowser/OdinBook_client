d703e78cf1927dd9f0138349e6b18cc903bea102
Dakota <prattdakota12@gmail.com>
2023-02-02 22:06:58 -0500

    simplified Nav render and changed links to buttons

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,41 +1,22 @@
 import React from 'react';
 import { Link, Navigate } from 'react-router-dom';
-import authService from '../services/auth.service';
 
 function Nav(props) {
-  const { user } = props;
-  console.log(user);
-  return (
-    <div className='nav-container'>
-      <ul>
+  const { loggedInUser, handlelogout } = props;
+
+  if (loggedInUser) {
+    return (
+      <div className='nav-container'>
         <Link to={'/'}>
-          <li>Home</li>
+          <button>Home</button>
         </Link>
-        {!user ? (
-          <Link to={'/login'}>
-            <li>Login/Sign-Up</li>
-          </Link>
-        ) : null}
-        {user ? (
-          <Link to={'/profile/' + user.id}>
-            <li>Profile</li>
-          </Link>
-        ) : null}
-        {user ? (
-          <Link>
-            <li
-              onClick={() => {
-                authService.logout();
-                <Navigate to={'/login'} />;
-              }}
-            >
-              Logout
-            </li>
-          </Link>
-        ) : null}
-      </ul>
-    </div>
-  );
+        <Link to={`/profile/${loggedInUser.id}`}>
+          <button>Profile</button>
+        </Link>
+        <button onClick={handlelogout}>Logout</button>
+      </div>
+    );
+  }
 }
 
 export default Nav;
