45b5700421a5ee747149ab3a8623f69ca8c82b11
Dakota <prattdakota12@gmail.com>
2023-02-03 20:43:44 -0500

    changed login to try/catch and added error handling

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,18 +1,35 @@
 import axios from 'axios';
 
-const API_URI = 'http://localhost:4000/api/auth';
+const API_AUTH_URI = 'http://localhost:4000/api/auth';
 
-const login = async (email, password) => {
-  return axios.post(API_URI + '/login', { email, password }).then((res) => {
-    // console.log(res.data);
-    localStorage.setItem('user', JSON.stringify(res.data.user));
-    return res.data;
-  });
+const loginAwait = async (email, password) => {
+  try {
+    const res = await axios({
+      method: 'post',
+      data: { email, password },
+      url: `${API_AUTH_URI}/login`,
+    });
+
+    if (res.status === 200) {
+      console.log(res);
+      localStorage.setItem('user', JSON.stringify(res.data.user));
+      return res.data;
+    }
+  } catch (error) {
+    console.log('ERRRR', error.response);
+    return error.response;
+  }
+
+  // return axios.post(API_URI + '/login', { email, password }).then((res) => {
+  //   // console.log(res.data);
+  //   localStorage.setItem('user', JSON.stringify(res.data.user));
+  //   return res.data;
+  // });
 };
 
 const signup = async (email, password, firstname, lastname) => {
   return axios
-    .post(API_URI + '/signup', { email, password, firstname, lastname })
+    .post(API_AUTH_URI + '/signup', { email, password, firstname, lastname })
     .then((res) => {
       console.log(res.data);
       return res.data;
@@ -28,6 +45,6 @@ const getCurrentUser = async () => {
   return JSON.parse(localStorage.getItem('user'));
 };
 
-const authService = { login, signup, logout, getCurrentUser };
+const authService = { loginAwait, signup, logout, getCurrentUser };
 
 export default authService;
